37c34d88180270149fc4de22078c1afbaa6ce8b8
Henorvell <comword@live.com>
2016-01-22 20:44:06 +0800

    perpare to remove golang

--- golang/src/httpserver/http.go
+++ golang/src/httpserver/http.go
@@ -2,7 +2,12 @@ package httpserver
 import (
   "C"
   "net/http"
+  "io"
+  "log"
+  "os"
 )
+var Logger *log.Logger
+var outFile *os.File
 func InitBinding(port string) string {
   //port := ":8080"
   routerBinding()
@@ -13,5 +18,24 @@ func InitBinding(port string) string {
   return "Success."
 }
 func routerBinding() int {
+  http.Handle("/html/", http.StripPrefix("/html/", http.FileServer(http.Dir("./html"))))
+  http.HandleFunc("/cgilogin", cgi_login)
   return 0
+}
+func Loginit(path string) {
+  if Logger == nil {
+    outFile, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+    if err != nil {
+      log.Fatalln("Failed to open log file", output, ":", err)
+    }
+    Logger = log.New(outFile, "", log.Ldate|log.Ltime|log.Llongfile)
+    Logger.Println("Log inited")
+  }
+}
+func CloseLogFile() {
+  outFile.Close()
+}
+func cgi_login(rw http.ResponseWriter, req *http.Request) {
+  io.WriteString(rw,"");
+  req.parseForm()
 }
